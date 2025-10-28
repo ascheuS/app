68feb6cd7004e494431d84e6a733fe2df875c35d
Tban1 <rojasesteban411@gmail.com>
2025-10-28 12:01:48 -0300

    feat: Ignorar config.ts y dejar de rastrearlo

--- mobile-app/src/constants/config.ts
+++
@@ -1,9 +0,0 @@
-export const API_CONFIG = {
-    BASE_URL: 'http://192.168.1.6:8000',
-    TIMEOUT: 10000, // 10 segundos
-};
-
-export const APP_CONFIG = {
-    APP_NAME: 'SIGRA Mobile',
-    VERSION: '1.0.0',
-};
