5b7ffe33911ad21b05d83dbaeb34e863c2fafa36
Tban1 <rojasesteban411@gmail.com>
2025-11-04 21:50:48 -0300

    ASASAAA

--- mobile-app/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    // ¡Asegúrate de que esta línea esté!
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-  ]
-};
